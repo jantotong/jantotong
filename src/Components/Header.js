7589822eb675d84947d4521a345faa558fb41c7e
jantotong <43649573+jantotong@users.noreply.github.com>
2021-06-18 17:56:28 -0700

    Alpha 0.01

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Header extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.location;
-      var email = this.props.data.email;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
+        if (this.props.data) {
+            var name = this.props.data.name;
+            var occupation = this.props.data.occupation;
+            var description = this.props.data.description;
+            var city = this.props.data.location;
+            var email = this.props.data.email;
+            var networks = this.props.data.social.map(function (network) {
+                return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+            })
+        }
+
+        return (
+            <header id="home">
+
+                <nav id="nav-wrap">
+                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                    <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+                    <ul id="nav" className="nav">
+                        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                        <li><a className="smoothscroll" href="#about">About</a></li>
+                        <li><a className="smoothscroll" href="#resume">Experience</a></li>
+                        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                    </ul>
+
+                </nav>
+
+                <div className="row banner">
+                    <div className="banner-text">
+                        <h1 className="responsive-headline">{name}</h1>
+                        <h2 className="responsive-headline"> San Jose State University Computer Science Graduate </h2>
+                        <br/>
+
+                        <h3 className="responsive-headline">Passionate {occupation} {description} {city}</h3>
+                        <hr/>
+                        <ul className="social">
+                            {networks}
+                        </ul>
+                    </div>
+                </div>
+
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+                </p>
+
+            </header>
+        );
     }
-
-    return (
-      <header id="home">
-
-      <nav id="nav-wrap">
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-         </ul>
-
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">{name}</h1>
-             <h2 className="responsive-headline"> San Jose State University Computer Science Graduate </h2>
-             <br />
-
-             <h3 className="responsive-headline">Passionate {occupation} {description} {city}</h3>
-            <hr />
-             <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
-
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
-    );
-  }
 }
 
 export default Header;
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,87 +1,78 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Resume extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-            <p className="city">{education.city}</p>
-            <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-            <br />
-       </div>
-      })
+        if (this.props.data) {
+            var skillmessage = this.props.data.skillmessage;
+            var education = this.props.data.education.map(function (education) {
+                return <div key={education.school}><h3>{education.school}</h3>
+                    <p className="city">{education.city}</p>
+                    <p className="info">{education.degree} <span>&bull;</span><em
+                        className="date">{education.graduated}</em></p>
+                    <br/>
+                </div>
+            })
 
-      var work = this.props.data.work.map(function(work){
-          var des_arr = work.description;
-          console.log(des_arr + "  HERE  " );
-          var des_list = work.description.map(function (single_des){
-              return <li>{single_des}</li>;
-          })
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <ul>{des_list}</ul>
-        </div>
-      })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+            var work = this.props.data.work.map(function (work) {
+                var des_arr = work.description;
+                var des_list = des_arr.map(function (single_des) {
+                    return <li>{single_des}</li>;
+                })
+                return <div key={work.company}><h3>{work.company}</h3>
+                    <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+                    <ul>{des_list}</ul>
+                </div>
+            })
+
+            var skills = this.props.data.skills.map(function (skill) {
+                return <div className="each_skill"><h1>{skill.title}</h1><img className="skill_image" src={"images/"+skill.image} alt={skill.alt}/><h2>{skill.description}</h2>
+                 </div>
+            })
+
+        }
+
+        return (
+            <section id="resume">
+
+                <div className="row education">
+                    <div className="three columns header-col">
+                        <h1><span>Education</span></h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        <div className="row item">
+                            <div className="twelve columns">
+                                {education}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="row work">
+
+                    <div className="three columns header-col">
+                        <h1><span>Work Experience</span></h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        {work}
+                    </div>
+                </div>
+
+
+                <div className="row skill">
+                    <div className="three columns header-col">
+                        <h1><span>Skills</span></h1>
+                    </div>
+                    <div id = "skills-wrapper" class="skills">
+                                {skills}
+                    </div>
+                </div>
+            </section>
+        );
     }
-
-    return (
-      <section id="resume">
-
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work Experience</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
-        </div>
-    </div>
-
-
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
-    );
-  }
 }
 
 export default Resume;
