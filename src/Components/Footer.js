f61f24fb74e1b088b120606173ac6d7000f3477c
jantotong <tongjantoo@gmail.com>
2024-07-14 20:51:26 -0400

    update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>Jan To Tong (2023)</li>
+              <li>Jan To Tong (2024)</li>
            </ul>
 
         </div>
